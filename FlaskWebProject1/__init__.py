7cd1d969bff048abccddbecb431e235b86d8a73d
Ben Chadwick <ben.chadwick@dublin.siteserv.ie>
2017-05-16 16:26:53 +0100

    Original API material weights to csv

--- FlaskWebProject1/__init__.py
+++ FlaskWebProject1/__init__.py
@@ -3,6 +3,18 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_mail import Mail
+import os
+
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') ## Testing Locally will require entering these or setting in virtual env
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['DEFAULT_MAIL_SENDER'] = 'Admin <engineeringgeofiles@gmail.com>'
+
+mail = Mail(app)
 
 import FlaskWebProject1.views
--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -3,8 +3,13 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, abort, jsonify
 from FlaskWebProject1 import app
+import pandas as pd
+from flask_mail import Message
+from __init__ import mail
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -16,6 +21,7 @@ def home():
         year=datetime.now().year,
     )
 
+
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
@@ -26,6 +32,7 @@ def contact():
         message='Your contact page.'
     )
 
+
 @app.route('/about')
 def about():
     """Renders the about page."""
@@ -35,3 +42,33 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+@app.route('/parse_geo_pal/davenport/material_to_csv', methods=['POST'])
+def materials_to_csv():
+    if not request.json:
+        abort(400)
+    csv_data = request.json
+    for material in csv_data:
+        material['total'] = float(material['total'])
+        material['weight'] = float(material['weight'])
+        material['amount'] = float(material['amount'])
+    materials_df = pd.DataFrame(csv_data)
+    materials_df.to_csv('FlaskWebProject1/static/files/materials_weight.csv')
+
+    total = materials_df['total'].sum()
+    email_address = "engineeringgeofile@gmail.com"
+
+    message = Message("Hello World!",
+                      sender=('Actavo-Engineering', "engineeringgeofile@gmail.com"),
+                      recipients=["ben.chadwick@actavo.com"])
+    message.body = "Material Weights Summary"
+    with app.open_resource('static/files/materials_weight.csv') as csv_file:
+        message.attach('materials_weight.csv', 'text/csv', csv_file.read())
+    try:
+        mail.send(message)
+        return jsonify({'TotalWeight': total,
+                        'EmailSent': 'true',
+                        'EmailAddress': email_address}), 200
+    except StandardError:
+        return jsonify({"Email": "Failure"})
